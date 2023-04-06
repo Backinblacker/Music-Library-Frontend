8129781838a22ffc3b2415c1fe85f491d4a0c8c7
Backinblacker <coachblacker@gmail.com>
2023-04-06 15:40:41 -0600

    📦 NEW: Axios framework built in App.js to connect to music library

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import NavBar from './Components/NavigationBar/NavigationBar';
 
 function App() {
-  return (
+  const [songs, setSongs] = useState([]);
+    useEffect(() =>{
+      getAllSongs();
+    }, [])
+    async function getAllSongs(){
+      let response = await axios.get('http://127.0.0.1:5000/api/songs')
+      setSongs(response.data)
+    }
+    async function createSong(newSong){
+        // newSong:
+        // {
+        //     title: "Arabian Nights",
+        //     album: "Aladdin",
+        //     artist: "Bruce Adler",
+        //     genre: "Pop",
+        //     release_date: "1992-11-10"
+        //     running_time: "79"
+        // }
+        let response = await axios.post('http://127.0.0.1:5000/api/songs', newSong)
+        if (response.status === 201){
+            await getAllSongs();
+        }
+    }
+    return (
     <div>
       <NavBar />
+
     </div>
   );
 }
