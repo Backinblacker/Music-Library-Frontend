c053c1179a157b772751cb58d35654949a1e5cff
Backinblacker <coachblacker@gmail.com>
2023-04-07 15:21:45 -0600

    🐛 FIX: added / to the http request and removed the default add song

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   }, []);
 
   async function getAllSongs() {
-    let response = await axios.get('http://127.0.0.1:5000/api/songs');
+    let response = await axios.get('http://127.0.0.1:5000/api/songs/');
     setSongs(response.data.songs);
     console.log(response.data.songs);
   }
@@ -23,15 +23,6 @@ function App() {
     console.log(response.data.songs);
   }
   async function createSong(newSong){
-    // newSong:
-    // {
-    //     title: "Arabian Nights",
-    //     album: "Aladdin",
-    //     artist: "Bruce Adler",
-    //     genre: "Pop",
-    //     release_date: "1992-11-10"
-    //     running_time: "79"
-    // }
     let response = await axios.post('http://127.0.0.1:5000/api/songs/', newSong)
     if (response.status === 201){
         await getAllSongs();
