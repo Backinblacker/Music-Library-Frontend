5bc585e3fa39fa436df7c9ae7621969f5201fce1
Backinblacker <coachblacker@gmail.com>
2023-04-12 10:46:25 -0600

    📦 NEW: Init Cypress Testing

---
+++ cypress/e2e/musicLibraryTest.cy.js
@@ -0,0 +1,11 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000')
+  });
+});
+
+//Testing Add Song
+
+//Testing Filter Songs
+
+//Testing Delete Song
