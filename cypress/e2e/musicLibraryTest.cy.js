fd58c2f809c417ef654b574d4bc45d892e4f0073
Backinblacker <coachblacker@gmail.com>
2023-04-12 13:07:07 -0600

    📦 NEW: Testing Add song works

--- cypress/e2e/musicLibraryTest.cy.js
+++ cypress/e2e/musicLibraryTest.cy.js
@@ -1,10 +1,33 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000')
+describe('Testing Music Project', () => {
+  describe('visit site', () =>{
+    it('passes', () => {
+      cy.visit('http://localhost:3000')
+      //Testing Add Song
+      // cy.get('[data-test=NewSong]').contains('Add New Song').click()
+      // cy.get('[name="title"]')
+      //   .type('The Nights')
+      //   .should('have.value', 'The Nights')
+      // cy.get('[name="album"]')
+      //   .type('Avicii')
+      //   .should('have.value', 'Avicii')
+      // cy.get('[name="artist"]')
+      //   .type('Avicii')
+      //   .should('have.value', 'Avicii')
+      // cy.get('[name="genre"]')
+      //   .type('House')
+      //   .should('have.value', 'House')
+      // cy.get('[name="release_date"]')
+      //   .type('2015-01-11')
+      //   .should('have.value', '2015-01-11')
+      // cy.get('[name="running_time"]')
+      //   .type('179')
+      //   .should('have.value', '179')
+        // cy.get('button').click()
+    });
   });
 });
 
-//Testing Add Song
+
 
 //Testing Filter Songs
 
